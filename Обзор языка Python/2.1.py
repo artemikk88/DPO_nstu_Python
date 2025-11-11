d731f12e98f76687b7814b5e62ca6d751ed79a4f
Artem Zubkov <artem_zubkov_90@mail.ru>
2025-11-11 18:28:05 +0700

    Fix some bugs and make some reports about completed themes

--- Обзор языка Python/2.1.py
+++ Обзор языка Python/2.1.py
@@ -1,4 +1,4 @@
 Length = float(input("Введите длину комнаты (м): "))
 Width = float(input("Введите ширину комнаты (м): "))
 Square = Length * Width
-print("Площадь комнаты", Square, "м^2")
+print("Площадь комнаты", round(Square, 3), "м^2")
---
+++ Обзор языка Python/3.5.py
@@ -0,0 +1,3 @@
+chislo_str = input("Введите число: ")
+list_cifr = list(map(int, chislo_str))
+print(sum(list_cifr))
